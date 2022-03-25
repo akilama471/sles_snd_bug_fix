fff2208b8c914da1b30eb683c29af6745deadb76
Akila Madusanka <akilamadusanka471@gmail.com>
2022-03-25 22:51:50 +0530

    initial commit

---
+++ source_code/runMod.bat
@@ -0,0 +1,3 @@
+cd ..\..\
+start cod4x18_dedrun.exe +set r_xassetnum "xmodel=1200" +set dedicated 2 +set net_ip 127.0.0.1 +set net_port 28965  +set fs_game "mods\source_code"  +set rcon_password "123456789" +exec server.cfg +set developer 1 +map mp_crossfire +map_rotate
+pause
